1c6e865503c3371666fda614d1753869eabadd08
Yuriy Zubaha <yuzub@ua.fm>
2018-02-01 03:25:47 +0200

    changing template and adding some content from YVSef

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function() {
   $(window).on('load resize', function() {
     $('.fill-screen').css('height', window.innerHeight - 91);
-    console.log(window.innerHeight);
+    // console.log(window.innerHeight);
   });
 
   // add Bootstrap's scrollspy
@@ -15,6 +15,13 @@ $(function() {
     $('html, body').stop().animate({
       scrollTop: $($(this).attr('href')).offset().top - 55
     }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  // show/hide course content
+  $('#course-eng').click(_ => {
+    // alert('clicked on couse-eng');
+    $('#course-content-eng').toggle();
     event.preventDefault();
   });
 });
