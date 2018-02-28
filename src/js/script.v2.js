0261d5159ad6e4d76b5fd5cf117f798c1f6bef44
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2018-02-28 18:29:13 +0200

    .....

--- src/js/script.v2.js
+++ src/js/script.v2.js
@@ -33,7 +33,7 @@ $(function () {
   })
 
   // smooth scrolling
-  $('nav a').bind('click', function () {
+  $('nav a[href^="#"]').bind('click', function () {
     $('html, body').stop().animate({
       scrollTop: $($(this).attr('href')).offset().top - 55
     }, 1500, 'easeInOutExpo');
